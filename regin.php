8dfb23cd2f8807c833d90dd25bf62a84599620c1
Sapunov-danil <sapunov.daniil13@gmail.com>
2026-03-05 20:06:13 +0500

    шаг1-6

--- regin.php
+++ regin.php
@@ -19,6 +19,7 @@
 		<title> Регистрация </title>
 		
 		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
+		<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
@@ -43,6 +44,7 @@
 					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
 					<div class = "sub-name">Повторите пароль:</div>
 					<input name="_passwordCopy" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
+					<center><div class="g-recaptcha" data-sitekey="6Ld3hVEsAAAAAG9Gj9iAC9jYPyRyIx341N93NXp_"></div></center>
 					
 					<a href="login.php">Вернуться</a>
 					<input type="button" class="button" value="Зайти" onclick="RegIn()" style="margin-top: 0px;"/>
@@ -69,12 +71,19 @@
 				if(_login != "") {
 					if(_password != "") {
 						if(_password == _passwordCopy) {
+							var captcha = grecaptcha.getResponse();
+							if(captcha.length == 0) {
+								alert("Пройдите проверку");
+								return;
+							}
+
 							loading.style.display = "block";
 							button.className = "button_diactive";
 							
 							var data = new FormData();
 							data.append("login", _login);
 							data.append("password", _password);
+							data.append("g-recaptcha-response", captcha);
 							
 							// AJAX запрос
 							$.ajax({
