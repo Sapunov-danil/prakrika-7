0bb2a797c973bc4b99499870247751ce5d8638d1
Sapunov-danil <sapunov.daniil13@gmail.com>
2026-03-09 10:24:49 +0500

    авторизационные логи

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -15,6 +15,20 @@
 	
 	if($id != -1) {
 		$_SESSION['user'] = $id;
+
+		$ip = $_SERVER['REMOTE_ADDR'];
+		$dateStart = date("Y-m-d H:i:s");
+
+		$sql = "INSERT INTO `session` (`IdUser`, `Ip`, `DateStart`, `DateNow`) VALUES ({$id}, '{$ip}', '{$dateStart}', '{$dateStart}')";
+		$mysqli->query($sql);
+
+		$sql = "SELECT `Id` FROM `session` WHERE `DateStart` = '{$dateStart}'";
+		$query = $mysqli->query($sql);
+		$read = $query->fetch_assoc();
+		$_SESSION["IdSession"] = $read["Id"];
+
+		$sql = "INSERT INTO `logs`(`Ip`, `IdUser`, `Date`, `TimeOnline`, `Event`) VALUES ('{$ip}', {$id}, '{$dateStart}', '00:00:00', 'Пользователь {$login} авторизовался'";
+		$mysqli->query($sql);
 	}
 	echo md5(md5($id));
 ?>
